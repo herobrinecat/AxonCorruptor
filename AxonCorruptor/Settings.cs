4b7e1d8ab2137f23d1408778f5722ae706bb2584
herobrinecat <unconfigured@null.spigotmc.org>
2025-04-09 20:54:05 -0500

    Added settings, disk cache, and fixed saving if you already saved.

---
+++ AxonCorruptor/Settings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AxonCorruptor
+{
+    public partial class Settings : Form
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void Settings_Load(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(checkBox1, "Disk Caching allows you to save the original file to the disk instead of being stored to RAM.\n\nMost of the time you don't need this, however if the file is large, then this would be recommended.");
+            checkBox1.Checked = Properties.Settings.Default.DiskCache;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.DiskCache = checkBox1.Checked;
+            Properties.Settings.Default.Save();
+            Close();
+        }
+    }
+}
